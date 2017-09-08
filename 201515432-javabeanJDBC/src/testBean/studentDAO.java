df32b0a912ec1c3433f36890b93d45911a341637
201515432 <201515432@DESKTOP-LJ6PHG4>
2017-09-08 13:54:23 +0900

    9월

---
+++ 201515432-javabeanJDBC/src/testBean/studentDAO.java
@@ -0,0 +1,14 @@
+package testBean;
+import java.sql.*;
+public class studentDAO {
+ public void insert(studentVO item) throws Exception {
+  String number = item.getNumber();
+  String name = item.getName();
+  Class.forName("com.mysql.jdbc.Driver");
+  Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/201515432","201515432","201515432");
+  PreparedStatement sql = conn.prepareStatement("insert into student values(?,?)");
+  sql.setString(1,number);
+  sql.setString(2,name);
+  sql.executeUpdate();
+ }
+}
---
+++ 201515432-javabeanJDBC/src/testBean/studentVO.java
@@ -0,0 +1,20 @@
+package testBean;
+//자바빈 
+public class studentVO {
+private String number;
+private String name;
+public String getNumber() {
+	return number;
+}
+public void setNumber(String number) {
+	this.number = number;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+
+}
